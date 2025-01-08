0f4fa13f350d6564739e74a5cb88ac6aa952c289
Bence Molnár <xjarekx@gmail.com>
2025-01-08 22:34:57 +0100

    Add dedicated class to wrap the main logic

--- src/ConsoleApp/Program.cs
+++ src/ConsoleApp/Program.cs
@@ -1,111 +1,57 @@
-﻿using ConsoleApp.Cloudflare;
+﻿using ConsoleApp;
+using ConsoleApp.Cloudflare;
 using MbUtils.Extensions.CommandLineUtils;
-using McMaster.Extensions.CommandLineUtils;
 using Microsft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using Polly;
 using Polly.Extensions.Http;
 using Refit;
 
-namespace ConsoleApp;
+var wrapper = new CommandLineApplicationWrapper<UpdateDnsCliApplication>(args);
 
-[Command("update-dns", Description = "Update the DNS record for a domain.")]
-class Program
+var appDataFolder = GetAppDataFolder();
+if (!Directory.Exists(appDataFolder)) Directory.CreateDirectory(appDataFolder);
+
+wrapper.HostBuilder.ConfigureAppConfiguration((context, builder) => {
+   builder.AddJsonFile(Path.Combine(appDataFolder, "config.json"), optional: true);
+   builder.AddInMemoryCollection([
+      new KeyValuePair<string, string?>("AppDataFolder", appDataFolder),
+   ]);
+});
+
+wrapper.HostBuilder.ConfigureServices((hostBuilderContext, services) =>
 {
-   static async Task<int> Main(string[] args)
-   {
-      var wrapper = new CommandLineApplicationWrapper<Program>(args);
+   AppConfiguration appConfig =
+      hostBuilderContext.Configuration.Get<AppConfiguration>() ?? new AppConfiguration();
 
-      var appDataFolder = GetAppDataFolder();
-      if (!Directory.Exists(appDataFolder)) Directory.CreateDirectory(appDataFolder);
-
-      wrapper.HostBuilder.ConfigureAppConfiguration((context, builder) => {
-         builder.AddJsonFile(Path.Combine(appDataFolder, "config.json"), optional: true);
-         builder.AddInMemoryCollection([
-            new KeyValuePair<string, string?>("AppDataFolder", appDataFolder),
-            ]);
-      });
-
-      wrapper.HostBuilder.ConfigureServices((hostBuilderContext, services) =>
-      {
-         AppConfiguration appConfig =
-            hostBuilderContext.Configuration.Get<AppConfiguration>() ?? new AppConfiguration();
-
-         services
-            .AddRefitClient<IPublicIpAddressResolver>()
-            .ConfigureHttpClient(client => client.BaseAddress = new Uri(appConfig.PublicIpAddressResolverBaseUrl));
+   services
+      .AddRefitClient<IPublicIpAddressResolver>()
+      .ConfigureHttpClient(client => client.BaseAddress = new Uri(appConfig.PublicIpAddressResolverBaseUrl));
          
-         services.AddRefitClient<ISlackNotifications>()
-            .ConfigureHttpClient(client => client.BaseAddress = new Uri(appConfig.Slack.WebhookUrl));
+   services.AddRefitClient<ISlackNotifications>()
+      .ConfigureHttpClient(client => client.BaseAddress = new Uri(appConfig.Slack.WebhookUrl));
 
-         var retryPolicy = HttpPolicyExtensions.HandleTransientHttpError().WaitAndRetryAsync(3, retryAttempt => TimeSpan.FromSeconds(retryAttempt * 10));
+   var retryPolicy = HttpPolicyExtensions.HandleTransientHttpError().WaitAndRetryAsync(3, retryAttempt => TimeSpan.FromSeconds(retryAttempt * 10));
 
-         services
-            .AddCloudflareApi(hostBuilderContext.Configuration, httpClientBuilder => httpClientBuilder.AddPolicyHandler(retryPolicy))
-            .AddSingleton<IDnsRecordsService, CloudflareDnsRecordsService>()
-            .AddKeyedSingleton<IIdMappings, FileCachedIdMappings>("cache")
-            .AddKeyedSingleton<IIdMappings, CloudflareApiIdMappings>("api");
+   services
+      .AddCloudflareApi(hostBuilderContext.Configuration, httpClientBuilder => httpClientBuilder.AddPolicyHandler(retryPolicy))
+      .AddSingleton<IDnsRecordsService, CloudflareDnsRecordsService>()
+      .AddKeyedSingleton<IIdMappings, FileCachedIdMappings>("cache")
+      .AddKeyedSingleton<IIdMappings, CloudflareApiIdMappings>("api");
 
-         services.AddOptions<AppConfiguration>()
-            .Bind(hostBuilderContext.Configuration);
+   services.AddOptions<AppConfiguration>()
+      .Bind(hostBuilderContext.Configuration);
          
-      });
+});
 
-      wrapper.HostBuilder.UseSerilogWithOpenTelemetry();
+wrapper.HostBuilder.UseSerilogWithOpenTelemetry();
 
-      return await wrapper.ExecuteAsync();
-   }
+return await wrapper.ExecuteAsync();
 
-   private static string GetAppDataFolder()
-   {
-      var homeFolder = Environment.GetEnvironmentVariable("HOME")
-         ?? Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-      return Path.Combine(homeFolder, ".update-dns");
-   }
-
-   [Argument(0, "domain", "The domain to update.")]
-   private string Domain { get; } = string.Empty;
-
-   [Argument(1, "name", "The name of the record to update.")]
-   private string Name { get; } = string.Empty;
-
-   public async Task<int> OnExecuteAsync(ILogger<Program> logger, IPublicIpAddressResolver publicIpAddressResolver, IDnsRecordsService dnsRecordsService, ISlackNotifications slackNotifications)
-   {
-      try
-      {
-         // Get the public IP address
-         logger.LogInformation("Getting public IP address...");
-         var publicIpAddress = await publicIpAddressResolver.GetPublicIpAddressAsync();
-         logger.LogInformation("Public IP address is '{IpAddress}'", publicIpAddress);
-
-         // Get the current address of the DNS record
-         logger.LogInformation("Getting current address of the DNS record...");
-         var addressInDnsRecord = await dnsRecordsService.GetCurrentAddressAsync(Domain, Name);
-         logger.LogInformation("Current address is '{IpAddress}'", addressInDnsRecord);
-
-         // If the alias is already set to the IP address, then we're done
-         if (addressInDnsRecord == publicIpAddress)
-         {
-            logger.LogInformation("Address is already set to the current IP address");
-            return 0;
-         }
-
-         // Call the domain registrar
-         logger.LogInformation("Updating address...");
-         await dnsRecordsService.UpdateRecordAsync(Domain, Name, publicIpAddress);
-         logger.LogInformation("Address updated.");
-         
-         // Send a notification
-         await slackNotifications.SendNotificationAsync(new SlackMessage($"DNS record for {Name}.{Domain} updated from {addressInDnsRecord} to {publicIpAddress}"));   
-         
-         return 0;
-      }
-      catch (Exception ex)
-      {
-         logger.LogError(ex, "An error occurred");
-         return -1;
-      }
-   }
+static string GetAppDataFolder()
+{
+   var homeFolder = Environment.GetEnvironmentVariable("HOME")
+                    ?? Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+   return Path.Combine(homeFolder, ".update-dns");
 }
---
+++ src/ConsoleApp/UpdateDnsCliApplication.cs
@@ -0,0 +1,52 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using Microsoft.Extensions.Logging;
+
+namespace ConsoleApp;
+
+[Command("update-dns", Description = "Update the DNS record for a domain.")]
+internal class UpdateDnsCliApplication
+{
+   [Argument(0, "domain", "The domain to update.")]
+   private string Domain { get; } = string.Empty;
+
+   [Argument(1, "name", "The name of the record to update.")]
+   private string Name { get; } = string.Empty;
+
+   public async Task<int> OnExecuteAsync(ILogger<UpdateDnsCliApplication> logger, IPublicIpAddressResolver publicIpAddressResolver, IDnsRecordsService dnsRecordsService, ISlackNotifications slackNotifications)
+   {
+      try
+      {
+         // Get the public IP address
+         logger.LogInformation("Getting public IP address...");
+         var publicIpAddress = await publicIpAddressResolver.GetPublicIpAddressAsync();
+         logger.LogInformation("Public IP address is '{IpAddress}'", publicIpAddress);
+
+         // Get the current address of the DNS record
+         logger.LogInformation("Getting current address of the DNS record...");
+         var addressInDnsRecord = await dnsRecordsService.GetCurrentAddressAsync(Domain, Name);
+         logger.LogInformation("Current address is '{IpAddress}'", addressInDnsRecord);
+
+         // If the alias is already set to the IP address, then we're done
+         if (addressInDnsRecord == publicIpAddress)
+         {
+            logger.LogInformation("Address is already set to the current IP address");
+            return 0;
+         }
+
+         // Call the domain registrar
+         logger.LogInformation("Updating address...");
+         await dnsRecordsService.UpdateRecordAsync(Domain, Name, publicIpAddress);
+         logger.LogInformation("Address updated.");
+         
+         // Send a notification
+         await slackNotifications.SendNotificationAsync(new SlackMessage($"DNS record for {Name}.{Domain} updated from {addressInDnsRecord} to {publicIpAddress}"));   
+         
+         return 0;
+      }
+      catch (Exception ex)
+      {
+         logger.LogError(ex, "An error occurred");
+         return -1;
+      }
+   }
+}
